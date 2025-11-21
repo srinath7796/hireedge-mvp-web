e43179a8e3886b3dd6af2f32cd1fe20f9a094eb2
Srinath Senthilkumar <145647472+srinath7796@users.noreply.github.com>
2025-11-21 17:02:24 +0000

    Create resume.js

---
+++ pages/resume.js
@@ -0,0 +1,8 @@
+export default function ResumePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Resume Generator (MVP)</h1>
+      <p>Here we will connect to /api/generate-resume from hireedge-backend-mvp.</p>
+    </main>
+  );
+}
